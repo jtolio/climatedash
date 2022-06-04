06ae508aa1ee694ec63cc9de9d7f949dace28d1e
JT Olio <hello@jtolio.com>
2022-06-04 09:04:30 -0600

    black formatting

--- data-gen/keep-trying-download.py
+++ data-gen/keep-trying-download.py
@@ -2,14 +2,19 @@
 
 import os, subprocess, time
 
+
 def filecount():
     return len([x for x in os.listdir("data") if x.endswith(".nc")])
 
+
 def allsame(vals):
-    if len(vals) == 0: return True
+    if len(vals) == 0:
+        return True
     for v in vals[1:]:
-        if v != vals[0]: return False
+        if v != vals[0]:
+            return False
     return True
+
 
 filecounts = [1, 2, 3]
 
